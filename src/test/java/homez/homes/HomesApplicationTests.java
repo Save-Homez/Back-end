5f378b9bd4262b5bf6d4b538b66a1d8166a17e08
hajungIm <ihj_ewha@ewhain.net>
2024-04-28 21:33:39 +0900

    init: 프로젝트 초기 세팅

---
+++ src/test/java/homez/homes/HomesApplicationTests.java
@@ -0,0 +1,13 @@
+package homez.homes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
