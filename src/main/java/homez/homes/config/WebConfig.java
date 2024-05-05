402dac40ce73aae1605a12c35f0f53e707de311d
hajungIm <ihj_ewha@ewhain.net>
2024-05-05 23:46:27 +0900

    fix: 도메인명 수정

--- src/main/java/homez/homes/config/WebConfig.java
+++ src/main/java/homez/homes/config/WebConfig.java
@@ -9,7 +9,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://save-homez.vercel.app/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://save-homez.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
