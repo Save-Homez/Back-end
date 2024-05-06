855799171ae80ed5b7c17877c02734592662261d
hajungIm <ihj_ewha@ewhain.net>
2024-05-06 17:59:45 +0900

    fix: 여러 도메인 주소인 경우 수정

--- src/main/java/homez/homes/config/WebConfig.java
+++ src/main/java/homez/homes/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("https://save-homez.vercel.app")
+                .allowedOrigins("http://localhost:3000", "https://save-homez.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true)
