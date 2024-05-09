fdf6d8397b42167e489e9d3371bb9a479b6c5712
hajungIm <ihj_ewha@ewhain.net>
2024-05-09 16:37:31 +0900

    fix: getCorsConfiguration 방식

--- src/main/java/homez/homes/config/SecurityConfig.java
+++ src/main/java/homez/homes/config/SecurityConfig.java
@@ -2,13 +2,12 @@ package homez.homes.config;
 
 import homez.homes.util.jwt.JwtTokenFilter;
 import homez.homes.util.jwt.JwtTokenUtils;
-import java.time.Duration;
-import java.util.Arrays;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
@@ -18,7 +17,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -35,7 +33,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("https://save-homez.vercel.app"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
@@ -48,17 +57,16 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("https://save-homez.vercel.app"));
-//        config.addAllowedOrigin("http://localhost:3000");
-        config.setAllowedMethods(Arrays.asList("GET", "POST"));
-        config.addAllowedHeader("*");
-        config.setAllowCredentials(true);
-        config.setMaxAge(Duration.ofSeconds(3600));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-        return source;
-    }
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList("https://save-homez.vercel.app"));
+////        config.addAllowedOrigin("http://localhost:3000");
+//        config.setAllowedMethods(Arrays.asList("GET", "POST"));
+//        config.addAllowedHeader("*");
+//        config.setAllowCredentials(true);
+//        config.setMaxAge(Duration.ofSeconds(3600));
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//        return source;
+//    }
 }
