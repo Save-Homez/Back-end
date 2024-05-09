c5f862c5fe88e5176063706f4d107a9e7465dd26
hajungIm <ihj_ewha@ewhain.net>
2024-05-09 16:26:54 +0900

    fix: 이름 정자로 변경

--- src/main/java/homez/homes/config/SecurityConfig.java
+++ src/main/java/homez/homes/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public CorsConfigurationSource corsConfigSource() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(Arrays.asList("https://save-homez.vercel.app"));
 //        config.addAllowedOrigin("http://localhost:3000");
